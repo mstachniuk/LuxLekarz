1f8a9640a4f8c3622b2618e29df604ef0724bdd0
Piotr Wielgolaski <pwielgolaski@gmail.com>
2019-03-01 20:58:13 +0100

    Fixes #5 by updating selectors and adding DOM listener for result page
    Also refresh code more to ES6 style and some refactoring to reduce
    code duplication.

--- src/myscript.js
+++ src/myscript.js
@@ -1,28 +1,28 @@
 function starImgGoldName(partNumber) {
-    if(partNumber % 2 === 0) {
+    if (partNumber % 2 === 0) {
         return "star-gold-left-10.png";
     }
     return "star-gold-right-10.png"
 }
 
 function starImgGrayName(partNumber) {
-    if(partNumber % 2 === 0) {
+    if (partNumber % 2 === 0) {
         return "star-gray-left-10.png";
     }
     return "star-gray-right-10.png"
 }
 
 function createStarScaleAsImgElements(numberOfStars) {
-    var text = "";
-    if(isNaN(numberOfStars)) {
+    let text = "";
+    if (isNaN(numberOfStars)) {
         numberOfStars = 0;
     }
-    var doubleNumberOfStarsInt = Math.ceil(numberOfStars * 2);
-    for (var i = 0; i < doubleNumberOfStarsInt; i++) {
+    const doubleNumberOfStarsInt = Math.ceil(numberOfStars * 2);
+    for (let i = 0; i < doubleNumberOfStarsInt; i++) {
         text = text + '<img src="' + chrome.extension.getURL(starImgGoldName(i)) +
             '" alt="star" width="5" height="10">';
     }
-    for (var j = doubleNumberOfStarsInt; j < 10; j++) {
+    for (let j = doubleNumberOfStarsInt; j < 10; j++) {
         text = text + '<img src="' + chrome.extension.getURL(starImgGrayName(j)) +
             '" alt="star" width="5" height="10">';
     }
@@ -30,175 +30,87 @@ function createStarScaleAsImgElements(numberOfStars) {
 }
 
 function addNoDataInfo(doctor) {
-    var doctorNameDiv = $(".reserveTable > tbody > tr > td[colspan=3] > div:nth-child(1)");
-    var doctorName = getDoctorName(doctor);
-    for (var i = 0; i < doctorNameDiv.length; i++) {
-        if (doctorNameDiv[i].textContent.trim() === doctorName) {
-            $(doctorNameDiv[i]).append("&nbsp;&nbsp;");
-            var linkAndStars = '<a href="https://www.znanylekarz.pl/" target="_blank" '
-                + 'title="Brak strony o danym lekarzu w serwisie znanylekarz.pl Kliknij, aby przejść do tej strony '
-                + 'i spróbuj wyszukać lekarza ręcznie."><span style="white-space: nowrap;">'
-                + createStarScaleAsImgElements(0) + '</span></a>';
-            $(doctorNameDiv[i]).append(linkAndStars);
-        }
-    }
-}
+    const linkAndStars = `<a href="https://www.znanylekarz.pl/" target="_blank" 
+        title="Brak strony o danym lekarzu w serwisie znanylekarz.pl Kliknij, aby przejść do tej strony i spróbuj wyszukać lekarza ręcznie.">
+        <span style="white-space: nowrap;">${createStarScaleAsImgElements(0)}</span></a>`;
+    doctor.addInformation(linkAndStars);
 
-function addStarsToStartPageFutureTerms(doctor, numberOfStars, url) {
-    var doctorNameDiv = $("#userReservations > div:nth-child(3) > div > div.description.limit-width > h4");
-    var doctorName = getDoctorName(doctor);
-    for (var i = 0; i < doctorNameDiv.length; i++) {
-        if (doctorNameDiv[i].textContent.trim() === doctorName) {
-            $(doctorNameDiv[i]).append("&nbsp;&nbsp;");
-            var linkAndStars = '<a href="' + url + '" target="_blank" '
-                + 'title="Przejdź do strony znanylekarz.pl, aby zobaczyć wszystkie opinie.">'
-                + '<span style="white-space: nowrap;">'
-                + createStarScaleAsImgElements(numberOfStars) + '</span></a>';
-            $(doctorNameDiv[i]).append(linkAndStars);
-        }
-    }
-}
-
-function addStarsToStartPageExecutedTerms(doctor, numberOfStars, url) {
-    var doctorNameDiv = $("#userReservations > div.content > div.item > div.description:not(.limit-width) > h4");
-    var doctorName = getDoctorName(doctor);
-    for (var i = 0; i < doctorNameDiv.length; i++) {
-        if (doctorNameDiv[i].textContent.trim() === doctorName) {
-            $(doctorNameDiv[i]).append("&nbsp;&nbsp;");
-            var linkAndStars = '<a href="' + url + '" target="_blank" '
-                + 'title="Przejdź do strony znanylekarz.pl, aby zobaczyć wszystkie opinie.">'
-                + '<span style="white-space: nowrap;">'
-                + createStarScaleAsImgElements(numberOfStars) + '</span></a>';
-            $(doctorNameDiv[i]).append(linkAndStars);
-        }
-    }
 }
 
 function addStarsToLuxPage(doctor, numberOfStars, url) {
-    if (isFindOrChangeTermSite()) {
-        var doctorNameDiv = $(".reserveTable > tbody > tr > td[colspan=3] > div:nth-child(1)");
-        var doctorName = getDoctorName(doctor);
-        for (var i = 0; i < doctorNameDiv.length; i++) {
-            if (doctorNameDiv[i].textContent.trim() === doctorName) {
-                $(doctorNameDiv[i]).append("&nbsp;&nbsp;");
-                var linkAndStars = '<a href="' + url + '" target="_blank" '
-                    + 'title="Przejdź do strony znanylekarz.pl, aby zobaczyć wszystkie opinie.">'
-                    + '<span style="white-space: nowrap;">'
-                    + createStarScaleAsImgElements(numberOfStars) + '</span></a>';
-                $(doctorNameDiv[i]).append(linkAndStars);
-            }
-        }
-    } else if (isVisitsPage()) {
-        var doctorNameDiv = $("#divReservedDynamicContent > div:nth-child(1) > div.description.limit-width > h4");
-        var doctorName = getDoctorName(doctor);
-        for (var i = 0; i < doctorNameDiv.length; i++) {
-            if (doctorNameDiv[i].textContent.trim() === doctorName) {
-                $(doctorNameDiv[i]).append("&nbsp;&nbsp;");
-                var linkAndStars = '<a href="' + url + '" target="_blank" '
-                    + 'title="Przejdź do strony znanylekarz.pl, aby zobaczyć wszystkie opinie.">'
-                    + '<span style="white-space: nowrap;">'
-                    + createStarScaleAsImgElements(numberOfStars) + '</span></a>';
-                $(doctorNameDiv[i]).append(linkAndStars);
-            }
-        }
-    } else if (isStartPage()) {
-        addStarsToStartPageFutureTerms(doctor, numberOfStars, url);
-        addStarsToStartPageExecutedTerms(doctor, numberOfStars, url);
-    }
+    const linkAndStars = `<a href="${url}" target="_blank" 
+        title="Przejdź do strony znanylekarz.pl, aby zobaczyć wszystkie opinie.">
+        <span style="white-space: nowrap;">${createStarScaleAsImgElements(numberOfStars)}</span>
+        </a>`;
+    doctor.addInformation(linkAndStars);
 }
 
 function loadDoctor(doctor) {
-    var arguments = {
-        params: getSearchParamForDoctor(doctor),
-        apiKey: "90a529da12d7e81ae6c1fae029ed6c8f",
-        appID: "docplanner"
-    };
-    var doctorResponse = $.post("https://docplanner-3.algolia.io/1/indexes/pl_autocomplete_doctor/query",
-        JSON.stringify(arguments));
-    doctorResponse.done(function (data) {
+    doctor.makeRequest().done(function (data) {
         if (data.hits.length > 0) {
-            var numberOfStars = parseFloat(data.hits[0].stars);
-            console.log("Odczytano " + numberOfStars + " gwiazdki dla: " + getDoctorName(doctor));
+            const numberOfStars = parseFloat(data.hits[0].stars);
+            console.log("Odczytano " + numberOfStars + " gwiazdki dla: " + doctor.name);
             addStarsToLuxPage(doctor, numberOfStars, data.hits[0].url.replace("http://", "https://"));
         } else {
-            console.log("Nie znaleziono strony dla: " + getDoctorName(doctor));
+            console.log("Nie znaleziono strony dla: " + doctor.name);
             addNoDataInfo(doctor);
         }
     });
 }
 
-function loadStars(doctors) {
-    doctors.forEach(loadDoctor)
+function loadStars(doctorNames, favour) {
+    for (let i = 0; i < doctorNames.length; i++) {
+        let doctor = new Doctor(doctorNames[i], favour[i].textContent.trim());
+        loadDoctor(doctor);
+    }
 }
 
 function isFindOrChangeTermSite() {
-    var currentUrl = window.location.toString();
-    return currentUrl === "https://portalpacjenta.luxmed.pl/PatientPortal/Reservations/Reservation/Find"
+    const currentUrl = window.location.toString();
+    return currentUrl === "https://portalpacjenta.luxmed.pl/PatientPortal/Reservations/Reservation/Search"
         || currentUrl === "https://portalpacjenta.luxmed.pl/PatientPortal/Reservations/Reservation/SearchVisitsForChangeTerm";
 }
 
 function isVisitsPage() {
-    var currentUrl = window.location.toString();
+    const currentUrl = window.location.toString();
     return currentUrl === "https://portalpacjenta.luxmed.pl/PatientPortal/Reservations/Visits";
 }
 
 function isStartPage() {
-    var currentUrl = window.location.toString();
+    const currentUrl = window.location.toString();
     return currentUrl === "https://portalpacjenta.luxmed.pl/PatientPortal/"
 }
 
 function startPageFutureTerms() {
-    // #userReservations > div:nth-child(3) > div:nth-child(2) > div.description.limit-width > h4
-    var doctorNames = $("#userReservations > div.content > div.item > div.description.limit-width > h4");
-    var favour = $("#userReservations > div > div > div.description > p:nth-child(2)");
-    var doctors = new Set();
-    for (var i = 0; i < doctorNames.length; i++) {
-        var doctor = createDoctor(doctorNames[i].textContent.trim(), favour[i].childNodes[0].textContent.trim());
-        doctors.add(doctor);
-    }
-
-    loadStars(doctors);
+    const doctorNames = $("#userReservations > div.content > div.item > div.description.limit-width > h4");
+    const favour = $("#userReservations > div > div > div.description > p:nth-child(2)").map((i, e) => e.childNodes[3]);
+    loadStars(doctorNames, favour);
 }
 
 function startPageExecutedTerms() {
-    // #userReservations > div:nth-child(4) > div:nth-child(2) > div.description > h4
-    var doctorNames = $("#userReservations > div.content > div.item > div.description:not(.limit-width) > h4");
-    var favour = $("#userReservations > div.content.with-padding-top > div.item > div.description ");
-    var doctors = new Set();
-    for (var i = 0; i < doctorNames.length; i++) {
-        var doctor = createDoctor(doctorNames[i].textContent.trim(), favour[i].childNodes[3].textContent.trim());
-        doctors.add(doctor);
-    }
-
-    loadStars(doctors);
+    const doctorNames = $("#userReservations > div.content > div.item > div.description:not(.limit-width) > h4");
+    const favour = $("#userReservations > div.content.with-padding-top > div.item > div.description ").map((i, e) => e.childNodes[3]);
+    loadStars(doctorNames, favour);
 }
 
 function start() {
     if (isFindOrChangeTermSite()) {
-        var doctorNames = $(".reserveTable > tbody > tr > td[colspan=3]:nth-child(2) > div:nth-child(1)");
-        var favour = $(".reserveTable > tbody > tr > td[colspan=3] > div:nth-child(2)");
-        var doctors = new Set();
-        for (var i = 0; i < doctorNames.length; i++) {
-            var doctor = createDoctor(doctorNames[i].textContent.trim(), favour[i].textContent.trim());
-            doctors.add(doctor);
-        }
-
-        loadStars(doctors);
+        $('#foundTermsDiv').bind('DOMSubtreeModified', function (e) {
+            if (e.target.localName === 'script') {
+                const doctorNames = $(".reserveTable > tbody > tr:not(:first-child) > td[colspan=3]:nth-child(2) > div:nth-child(1)");
+                const favour = $(".reserveTable > tbody > tr:not(:first-child) > td[colspan=3] > div:nth-child(2)");
+                loadStars(doctorNames, favour);
+            }
+        });
     } else if (isVisitsPage()) {
         //TODO: Split to Future Terms and Executed Terms
-        var doctorNames = $("#divReservedDynamicContent > div > div.description.limit-width > h4");
-        var favour = $("#divReservedDynamicContent > div > div.description.limit-width > p:nth-child(2)");
-        var doctors = new Set();
-        for (var i = 0; i < doctorNames.length; i++) {
-            var doctor = createDoctor(doctorNames[i].textContent.trim(), favour[i].childNodes[0].textContent.trim());
-            doctors.add(doctor);
-        }
-
-        loadStars(doctors);
+        const doctorNames = $("#divReservedDynamicContent > div > div.description.limit-width > h4");
+        const favour = $("#divReservedDynamicContent > div > div.description.limit-width > p:nth-child(2)").map((i, e) => e.childNodes[0]);
+        loadStars(doctorNames, favour);
     } else if (isStartPage()) {
-        var loadingText = $("#VisitBoxes > div > h4");
+        const loadingText = $("#VisitBoxes > div > span");
         if (loadingText.length > 0
-                && loadingText[0].textContent.trim() === "Ładowanie treści, proszę czekać") {
+            && loadingText[0].textContent.trim() === "Ładowanie treści, proszę czekać") {
             setTimeout(start, 1000);
             return;
         }
